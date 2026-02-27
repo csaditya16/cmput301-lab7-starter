5eb4aae921fffc469d1cbcf2766345b61a4b4026
Aditya Gyawali <gyawaliaditya16@gmail.com>
2026-02-27 16:05:42 -0700

    fixed

--- app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
@@ -1,20 +1,35 @@
 package com.example.androiduitesting;
 
 
+
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+
+
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.runner.RunWith;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+
 
 import androidx.test.espresso.action.ViewActions;
 
-import org.junit.Test;
-import static androidx.test.espresso.Espresso.onData;
-import static org.hamcrest.Matchers.anything;
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+    // Create the actual 'screen'
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
 
     public void addCity(String cityName){
 
